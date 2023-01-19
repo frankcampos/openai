3dcb8c06580baf6f74c1997b84c1696bdd32c9a4
frank <frankcampos85@gmail.com>
2023-01-18 20:24:00 -0600

    Initial commit

---
+++ comienzo.py
@@ -0,0 +1,31 @@
+
+import openai
+
+openai.api_key = "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"
+
+# List of countries that speak Spanish
+countries_in_spanish = ["Argentina", "Bolivia", "Chile", "Colombia", "Costa Rica", "Cuba", "Dominican Republic", 
+                       "Ecuador", "El Salvador", "Equatorial Guinea", "Guatemala", "Honduras", "Mexico", 
+                       "Nicaragua", "Panama", "Paraguay", "Peru", "Puerto Rico", "Spain", "Uruguay", "Venezuela"]
+
+# Create a list of prompts
+prompts = ["What is the capital of " + country + "?" for country in countries_in_spanish]
+
+# Initialize an empty list to store the answers
+answers = []
+
+# Iterate through the list of prompts
+for p in prompts:
+    # Use the OpenAI API to generate a text completion for each prompt
+    completions = openai.Completion.create(engine="text-davinci-002", prompt=p, max_tokens=1024)
+    # Append the answer to the list of answers
+    answers.append(completions.choices[0].text)
+
+# Print the list of answers
+print(answers)
+
+
+
+
+
+
